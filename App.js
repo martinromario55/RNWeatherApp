352a6457feca3411faca6e7aed5334062ca61751
martinromario55 <martinromario55@gmail.com>
2024-02-09 15:54:59 +0300

    Entire project

--- App.js
+++ App.js
@@ -1,13 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useEffect, useState } from 'react'
 
 export default function App() {
+  // Weather API
+  const [city, setCity] = useState(null)
+  useEffect(() => {
+    const getWeather = async () => {
+      const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Nairobi'
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key':
+            '5f7d14196dmsh85ec3a330be2b4ap160202jsn2c38f1092b0c',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        },
+      }
+
+      try {
+        const response = await fetch(url, options)
+        const result = await response.json()
+        setCity(result)
+        console.log(result)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getWeather()
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      {city && (
+        <ImageBackground
+          source={{
+            uri: city.current.is_day
+              ? 'https://bit.ly/4bzYX30'
+              : 'https://bit.ly/3HRzjsy',
+          }}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.header}>
+            <Text style={styles.headerText}>{city.location.name}</Text>
+            <Feather name="settings" size={24} color="#fff" />
+          </View>
+
+          <View style={styles.temp}>
+            <Text style={styles.tempShown}>{`${city.current.temp_c}°`}</Text>
+            <Text style={styles.tempText}>{city.current.condition.text}</Text>
+          </View>
+
+          <View style={styles.condition}>
+            <Text
+              style={styles.feels}
+            >{`Feels like ${city.current.feelslike_c}°`}</Text>
+            <Text style={styles.weather}>
+              <Feather name="wind" size={24} color="#fff" />
+              {`${city.current.wind_kph}Kph`}
+            </Text>
+          </View>
+        </ImageBackground>
+      )}
+      {Platform.OS === 'ios' && <StatusBar style="dark" />}
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +80,53 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  headerText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  temp: {
+    alignItems: 'center',
+  },
+  tempShown: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 140,
+  },
+  tempText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  condition: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 32,
+    paddingHorizontal: 10,
+    paddingVertical: 70,
+    marginTop: 32,
+  },
+  feels: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  weather: {
+    color: '#fff',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+})
